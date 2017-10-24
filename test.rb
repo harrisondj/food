188ed4fb341a610a3921ee8f40f952963099c001
Derek Harrison <hderek18@comcast.net>
2017-10-24 11:15:21 -0600

    Implement Meal

--- test.rb
+++ test.rb
@@ -1,5 +1,16 @@
 require "./food"
+require "./meal"
 
-scambled_eggs = Food.new("Scambled Eggs", "1 egg", 100)
+scrambled_eggs = Food.new("Scambled Eggs", "1 egg", 100)
+bacon = Food.new("Bacon", "3 slices", 100)
+cereal = Food.new("Cereal", "1 oz", 135)
 
-puts scambled_eggs
+# puts scambled_eggs
+
+breakfast = Meal.new("breakfast")
+breakfast.add_food(scrambled_eggs, 3)
+breakfast.add_food(bacon, 2)
+breakfast.add_food(Food.new("Cereal", "1 oz", 135), 2)
+
+puts breakfast
+puts "Breakfast calories: #{breakfast.calories}"
